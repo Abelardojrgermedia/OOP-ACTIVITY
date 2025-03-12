a669a0e9a3248473ad1a2867cae8be210a063459
Dell <Dell@DESKTOP-3U21K2Q>
2025-03-12 06:35:24 -0400

    Add project files.

---
+++ InsertNewProduct.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using OOP;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace OOP
+
+{
+    class ManageProduct
+    {
+        public class InsertNewProduct
+        {
+            private string server = "localhost";
+            private string database = "csharpapp";
+            private string username = "root";
+            private string password = "";
+            private string connString;
+
+            public InsertNewProduct()
+            {
+                connString = $"Server={server};Database={database};User ID={username};Password={password};";
+            }
+
+            public void InsertData(string Product_Name, int Product_Price, string Product_Description)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        Console.WriteLine("Connected to MySQL!");
+
+                        string query = "INSERT INTO products (Product_Name, Product_Price, Product_Description) VALUES (@name, @price, @description)";
+
+                        using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@name", Product_Name);
+                            cmd.Parameters.AddWithValue("@price", Product_Price);
+                            cmd.Parameters.AddWithValue("@description", Product_Description);
+
+                            int rowsAffected = cmd.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+                                Console.WriteLine(@" 
+                                                                                                                                        
+ ____    _  _____  _      ___ _   _ ____  _____ ____ _____ _____ ____    ____  _   _  ____ ____ _____ ____  _____ _   _ _     _  __   __
+|  _ \  / \|_   _|/ \    |_ _| \ | / ___|| ____|  _ |_   _| ____|  _ \  / ___|| | | |/ ___/ ___| ____/ ___||  ___| | | | |   | | \ \ / /
+| | | |/ _ \ | | / _ \    | ||  \| \___ \|  _| | |_) || | |  _| | | | | \___ \| | | | |  | |   |  _| \___ \| |_  | | | | |   | |  \ V / 
+| |_| / ___ \| |/ ___ \   | || |\  |___) | |___|  _ < | | | |___| |_| |  ___) | |_| | |__| |___| |___ ___) |  _| | |_| | |___| |___| |  
+|____/_/   \_|_/_/   \_\ |___|_| \_|____/|_____|_| \_\|_| |_____|____/  |____/ \___/ \____\____|_____|____/|_|    \___/|_____|_____|_|  
+                                                                                                                                        
+");
+                            }
+                            else
+                            {
+                                Console.WriteLine(@" 
+                                                                                                           
+ _____ _    ___ _     _____ ____    _____ ___    ___ _   _ ____  _____ ____ _____   ____    _  _____  _    
+|  ___/ \  |_ _| |   | ____|  _ \  |_   _/ _ \  |_ _| \ | / ___|| ____|  _ |_   _| |  _ \  / \|_   _|/ \   
+| |_ / _ \  | || |   |  _| | | | |   | || | | |  | ||  \| \___ \|  _| | |_) || |   | | | |/ _ \ | | / _ \  
+|  _/ ___ \ | || |___| |___| |_| |   | || |_| |  | || |\  |___) | |___|  _ < | |   | |_| / ___ \| |/ ___ \ 
+|_|/_/   \_|___|_____|_____|____/    |_| \___/  |___|_| \_|____/|_____|_| \_\|_|   |____/_/   \_|_/_/   \_\
+                                                                                                           
+");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(@" 
+                               
+ _____ ____  ____   ___  ____  
+| ____|  _ \|  _ \ / _ \|  _ \ 
+|  _| | |_) | |_) | | | | |_) |
+| |___|  _ <|  _ <| |_| |  _ < 
+|_____|_| \_|_| \_\\___/|_| \_\
+                               
+ " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using OOP;
+
+class MainProgram
+{
+    public static void Main()
+    {
+        OOP.ManageProduct.InsertNewProduct newProduct = new OOP.ManageProduct.InsertNewProduct();
+
+        string Product_Name;
+        string Product_Description;
+        int Product_Price;
+
+        int limit = 1;
+        int productCount = 0;
+
+        while (productCount < limit)
+        {
+            Console.Clear();
+            Console.WriteLine(@"
+                                                                                              
+ ___ _   _ ____  _____ ____ _____ ___ _   _  ____   ____  ____   ___  ____  _   _  ____ _____ 
+|_ _| \ | / ___|| ____|  _ |_   _|_ _| \ | |/ ___| |  _ \|  _ \ / _ \|  _ \| | | |/ ___|_   _|
+ | ||  \| \___ \|  _| | |_) || |  | ||  \| | |  _  | |_) | |_) | | | | | | | | | | |     | |  
+ | || |\  |___) | |___|  _ < | |  | || |\  | |_| | |  __/|  _ <| |_| | |_| | |_| | |___  | |  
+|___|_| \_|____/|_____|_| \_\|_| |___|_| \_|\____| |_|   |_| \_\\___/|____/ \___/ \____| |_|  
+                                                                                              
+");
+
+            Console.WriteLine(" ___________________________________________________________________________");
+            Console.WriteLine($"|\t\t\t\t\t\t\t\t\t    |\n| Input A Products [{productCount + 1}] You can Insert less than {limit} products:\t\t    |");
+            Console.WriteLine("|___________________________________________________________________________|");
+
+            Console.WriteLine("\n>>>>>>>>>>>>>>>> PRODUCT NAME <<<<<<<<<<<<<<<");
+            Console.WriteLine(@"Input the product name [max 25 character]");
+            Product_Name = Console.ReadLine();
+            if (Product_Name.Length > 25)
+            {
+                Console.WriteLine("Input a product at least less than 25 characters. Please Try Again!");
+                continue;
+            }
+
+            Console.WriteLine("\n>>>>>>>>>>>>>>>> PRODUCT PRICE <<<<<<<<<<<<<<<");
+            Console.WriteLine("Input the product price [at least 1 to 1,000]:");
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out Product_Price) && Product_Price >= 1 && Product_Price <= 1000)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid price. Please enter a price less than 1,000.");
+                }
+            }
+
+            Console.WriteLine("\n>>>>>>>>>>>>>>>> PRODUCT DESCRIPTION <<<<<<<<<<<<<<<");
+            Console.WriteLine("Input the product description [max 50 characters]");
+            Product_Description = Console.ReadLine();
+            if (Product_Description.Length > 50)
+            {
+                Console.WriteLine("Invalid description! Please enter a description with less than 50 characters.");
+                continue;
+            }
+
+            newProduct.InsertData(Product_Name, Product_Price, Product_Description);
+
+            productCount++;
+
+            
+
+            Console.WriteLine(" ___________________________________________________________________________");
+            Console.WriteLine($"|\t\t\t\t\t\t\t\t\t    |\n|\t\tYou have {limit - productCount} more products you can add.\t\t\t    |");
+            Console.WriteLine("|___________________________________________________________________________|");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        
+        Console.WriteLine(@"
+                                                                                                                                                                                                       
+  ___   ___  ____  ____  _  __   _____  _   _   ____  _____    _    ____ _   _   _     ___ __  __ ___ _____   ____  ____   ___  ____  _   _  ____ _____   ___ _   _ ____  _____ ____ _____ _____ ____  
+ / _ \ / _ \|  _ \/ ___|| | \ \ / / _ \| | | | |  _ \| ____|  / \  / ___| | | | | |   |_ _|  \/  |_ _|_   _| |  _ \|  _ \ / _ \|  _ \| | | |/ ___|_   _| |_ _| \ | / ___|| ____|  _ |_   _| ____|  _ \ 
+| | | | | | | |_) \___ \| |  \ V | | | | | | | | |_) |  _|   / _ \| |   | |_| | | |    | || |\/| || |  | |   | |_) | |_) | | | | | | | | | | |     | |    | ||  \| \___ \|  _| | |_) || | |  _| | | | |
+| |_| | |_| |  __/ ___) |_|   | || |_| | |_| | |  _ <| |___ / ___ | |___|  _  | | |___ | || |  | || |  | |   |  __/|  _ <| |_| | |_| | |_| | |___  | |    | || |\  |___) | |___|  _ < | | | |___| |_| |
+ \___/ \___/|_|   |____/(_)   |_| \___/ \___/  |_| \_|_____/_/   \_\____|_| |_| |_____|___|_|  |_|___| |_|   |_|   |_| \_\\___/|____/ \___/ \____| |_|   |___|_| \_|____/|_____|_| \_\|_| |_____|____/ 
+                                                                                                                                                                                                       
+");
+        
+    }
+}
